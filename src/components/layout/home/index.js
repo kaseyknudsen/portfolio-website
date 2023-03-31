f2186083c223e3cb35aefd40d0c9cb06616ea1f7
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-31 10:44:04 -0700

    added animated letters

--- src/components/layout/home/index.js
+++ src/components/layout/home/index.js
@@ -1,17 +1,50 @@
 import './index.scss'
 import LogoTitle from '../../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../../animatedLetters'
+import { useState } from 'react'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [' ', 'K', 'a', 's', 'e', 'y', '.']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.'
+  ]
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          lobodan
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+           <br /> 
+           <span className={`${letterClass} _13`}>I</span>
+           <span className={`${letterClass} _14`}>'m</span>
+          
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
-          web developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend Developer / Javascript / React</h2>
         <Link to="/contact" className="flat-button">
--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -13,8 +13,8 @@ const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
-        <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        {/* <img src={LogoS} alt="logo" /> */}
+        {/* <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
