28e1fbe3d859696fed0104f4dd5256c333ab8dc8
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-09 01:18:29 -0700

    added 3 images to projects page

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 import AnimatedLetters from '../animatedLetters'
 import { Link } from 'react-router-dom'
 import JazzImprovImage1 from '../../assets/images/jazz-improv-website-image-1.png'
+import JazzImprovImage2 from '../../assets/images/jazz-improv-website-image-2.png'
+import JazzImprovImage3 from '../../assets/images/jazz-improv-website-image-3.png'
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -19,7 +21,6 @@ const Projects = () => {
   return (
     <>
       <div className="container projects-page">
-      <img src={JazzImprovImage1} alt='Theory 1' className='theory-image'/>
         <div className="text-zone">
           <h1>
             <AnimatedLetters
@@ -28,16 +29,22 @@ const Projects = () => {
               idx={15}
             />
           </h1>
+         
           <div>
+          
             <p>
-              I built this website as a teaching tool for me and to help my saxophone students learn improvisation
-              and theory. It features a theory page that resembles flashcards with questions
-              and answers and an instrument-specific ear training page to help
-              students identify intervals. The site was built using React, Bootstrap, CSS, Formik,
-              EmailJS, Logic Pro, and is deployed using Vercel.
+              I built this website as a teaching tool for me and to help my
+              saxophone students learn improvisation and theory. It features a
+              theory page that resembles flashcards with questions and answers
+              and an instrument-specific ear training page to help students
+              identify intervals. The site was built using React, Bootstrap,
+              CSS, Formik, EmailJS, Logic Pro, and is deployed using Vercel.
             </p>
-            
-
+            <div className="theory-images-container">
+            <img src={JazzImprovImage2} alt="Theory 1" />
+            <img src={JazzImprovImage3} alt="Theory 1" />
+            <img src={JazzImprovImage1} alt="Theory 1" />
+          </div>
             <Link
               to="https://kaseyknudsenjazztheory.vercel.app/"
               className="flat-button"
