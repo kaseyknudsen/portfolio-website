dce271d85b7cec4911618defb1295f2ffa7ae79e
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-09 22:21:17 -0700

    updated text on projects page

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -26,18 +26,55 @@ const Projects = () => {
             <AnimatedLetters
               letterClass={letterClass}
               strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
-              idx={15}
+              idx={20}
             />
           </h1>
 
           <div>
+            <div style={{ marginTop: '5em' }}>
+              <h2>
+                <AnimatedLetters
+                  letterClass={letterClass}
+                  strArray={[
+                    'J',
+                    'a',
+                    'z',
+                    'z',
+                    ' ',
+                    'I',
+                    'm',
+                    'p',
+                    'r',
+                    'o',
+                    'v',
+                    ' ',
+                    'W',
+                    'e',
+                    'b',
+                    's',
+                    'i',
+                    't',
+                    'e',
+                  ]}
+                  idx={15}
+                />
+              </h2>
+            </div>
             <p>
               I built this website as a teaching tool for me and to help my
-              saxophone students learn improvisation and theory. It features a
-              theory page that resembles flashcards with questions and answers
-              and an instrument-specific ear training page to help students
-              identify intervals. The site was built using React, Bootstrap,
-              CSS, Formik, EmailJS, Logic Pro, and is deployed using Vercel.
+              saxophone students to learn improvisation. It features a theory
+              page that resembles flashcards with questions and answers and an
+              instrument-specific ear training page to help students identify
+              intervals.
+            </p>
+            <p>
+              This site has been an extremely helpful project for me to hone my
+              React and Javascript skills, and has given me the opportunity to
+              continuously test the usability in real-life teaching scenarios.
+              It is a constant work in progress, and has improved as I have
+              improved as a developer. The site was built using React,
+              Bootstrap, CSS, Formik, EmailJS, Logic Pro, and is deployed using
+              Vercel.
             </p>
             <div className="theory-images-container">
               <img src={JazzImprovImage3} alt="Theory 1" />
@@ -49,16 +86,48 @@ const Projects = () => {
               className="flat-button"
               target="_blank"
             >
-              JAZZ IMPROV WEBSITE
+              LIVE WEBSITE
             </Link>
           </div>
           <div style={{ marginTop: '100px' }}>
+            <div style={{ marginTop: '5em' }}>
+              <h2>
+                <AnimatedLetters
+                  letterClass={letterClass}
+                  strArray={[
+                    'D',
+                    'a',
+                    'n',
+                    'i',
+                    'c',
+                    'a',
+                    "'",
+                    's',
+                    ' ',
+                    'C',
+                    'u',
+                    's',
+                    't',
+                    'o',
+                    'm',
+                    ' ',
+                    'B',
+                    'a',
+                    'k',
+                    'e',
+                    's',
+                  ]}
+                  idx={15}
+                />
+              </h2>
+            </div>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
-              volutpat ac tincidunt vitae. Malesuada pellentesque elit eget
-              gravida cum sociis natoque penatibus et. Purus in massa tempor nec
-              feugiat nisl pretium.
+              Danica's Custom Bakes was founded by Danica Cortez on July 2022 in
+              Austin, TX. I started working as an intern on this project in
+              January of 2023. It has given me a chance to get my feet wet
+              designing UI Components, writing tests and working with a team of
+              developers. It has given me experience with Next JS, Material UI,
+              Cypress, Typescript, Mongo DB, Prisma, Docker, and Postman.
             </p>
 
             <Link
